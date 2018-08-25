eda660c497228228911cda7919c230f3a2f32d5c
Vincenzo Palazzo <vinci96-@live.it>
2018-08-25 11:59:27 +0200

    fix support font arabic

--- src/main/java/mdlaf/components/separator/MaterialSeparatorUI.java
+++ src/main/java/mdlaf/components/separator/MaterialSeparatorUI.java
@@ -7,6 +7,10 @@ import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.basic.BasicSeparatorUI;
 import java.awt.Graphics;
 
+/**
+ * @author https://github.com/vincenzopalazzo
+ */
+
 public class MaterialSeparatorUI extends BasicSeparatorUI {
 
 	public static ComponentUI createUI (JComponent c) {
--- src/main/java/mdlaf/utils/MaterialResurceManager.java
+++ src/main/java/mdlaf/utils/MaterialResurceManager.java
@@ -3,13 +3,16 @@ package mdlaf.utils;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+/**
+ * @author https://github.com/vincenzopalazzo
+ */
 public class MaterialResurceManager {
 
     private ResourceBundle resourceBundle;
 
     public void inizializza(){
-        //resourceBundle = ResourceBundle.getBundle("fontsProperties.fonts", new Locale("ar", "MA")); test
-        resourceBundle = ResourceBundle.getBundle("fontsProperties.fonts", Locale.getDefault());
+        resourceBundle = ResourceBundle.getBundle("fontsproperties.fonts", new Locale("ar", "MA")); //test
+        //resourceBundle = ResourceBundle.getBundle("fontsproperties.fonts", Locale.getDefault());
     }
 
     public String getStringResource(String key) {
