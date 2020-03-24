e356e9ebf6bb9238e4e331476cb4e2d65dc6d9e2
jarekstempien <jarek@insys.be>
2020-03-24 16:42:35 +0100

    Text components style Line/Outline

---
+++ src/main/java/mdlaf/shadows/TextFieldStyle.java
@@ -0,0 +1,11 @@
+package mdlaf.shadows;
+
+import java.awt.Color;
+
+import mdlaf.utils.MaterialConstants;
+
+public interface TextFieldStyle {
+	public MaterialConstants.TextComponent getTextFieldStyle();
+	public Color getDisabledBackground();
+	public Color getColorLine();
+}
